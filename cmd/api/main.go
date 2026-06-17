4c77b57e8d27c9315450b32fab1f922dd1641f73
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2026-06-17 13:17:12 +0530

    database config
    Signed-off-by: PARTH-TUSSLE <parthgartan26feb@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"REST-api/internal/config"
+	"REST-api/internal/database"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+
+	var cfg *config.Config
+	var err error
+
+	cfg, err = config.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
+	var pool *pgxpool.Pool
+
+	pool, err = database.Connect(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Failed to connect to DB:", err)
+	}
+
+	defer pool.Close()
+
 	var router *gin.Engine = gin.Default()
 	
 	router.SetTrustedProxies(nil)
@@ -13,8 +38,9 @@ func main() {
 		ctx.JSON(200, gin.H{
 			"message": "API server is running",
 			"status": "SUCCESS !",
+			"database": "connected !",
 		})
 	} )
 
-	router.Run(":8080")
+	router.Run(":"+ cfg.Port)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseURL string
+	Port        string
+}
+
+func Load() (*Config, error) {
+
+	var err error = godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Warning: .env file not found, using environment variables !")
+	}
+
+	var config *Config = &Config{
+		DatabaseURL: os.Getenv("DATABASE_URL"),
+		Port: os.Getenv("PORT"),
+	}
+
+	return config, nil
+
+}
